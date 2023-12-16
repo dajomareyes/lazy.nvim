6c8c61347be85b1b09abc984101cd5a4670bb2f5
David Reyes <dajo@Davids-MacBook-Pro.local>
2023-12-16 09:52:50 -0500

    add pre install script

---
+++ preinstall.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# requires homebrew is installed
+
+brew install wget
+brew install curl 
+brew install unzip 
+brew install git 
+brew install make
+brew install pip
+brew install python
+brew install npm 
+brew install node
+brew install luarocks
+brew install fd
+brew install ripgrep
+brew install cargo
