5222fe29e561351659897067566f55f3ee595758
David Reyes <david.reyes@vestwell.com>
2024-05-07 16:24:36 -0400

    add ability to toggle autoformat

--- lua/dajoma/plugins/formatting.lua
+++ lua/dajoma/plugins/formatting.lua
@@ -29,13 +29,12 @@ return {
         python = { "isort", "black" },
         groovy = { "npm-groovy-lint" },
       },
-      format_on_save = function()
-        show_notification("File formatted!", "info")
-        return {
-          lsp_fallback = true,
-          async = false,
-          timeout_ms = 1000,
-        }
+      format_on_save = function(bufnr)
+        -- Disable with a global or buffer-local variable
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+          return
+        end
+        return { timeout_ms = 500, lsp_fallback = true }
       end,
     })
 
@@ -46,5 +45,27 @@ return {
         timeout_ms = 1000,
       })
     end, { desc = "Format file or range (in visual mode)" })
+
+    vim.api.nvim_create_user_command("FormatToggle", function(args)
+      local is_global = not args.bang
+      if is_global then
+        vim.g.disable_autoformat = not vim.g.disable_autoformat
+        if vim.g.disable_autoformat then
+          show_notification("Autoformat-on-save disabled globally", "info")
+        else
+          show_notification("Autoformat-on-save enabled globally", "info")
+        end
+      else
+        vim.b.disable_autoformat = not vim.b.disable_autoformat
+        if vim.b.disable_autoformat then
+          show_notification("Autoformat-on-save disabled for this buffer", "info")
+        else
+          show_notification("Autoformat-on-save enabled for this buffer", "info")
+        end
+      end
+    end, {
+      desc = "Toggle autoformat-on-save",
+      bang = true,
+    })
   end,
 }
