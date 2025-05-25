8765dd1861336e19268a7d18bd6777f6a186cd4f
David Joshua Reyes <davidjoshuareyes@gmail.com>
2025-05-25 08:48:57 -0400

    spiderman

--- lua/dajoma/core/options.lua
+++ lua/dajoma/core/options.lua
@@ -1,43 +1,48 @@
 local opt = vim.opt -- for conciseness
 
+-- remove the banner for the file explorer 
+vim.cmd("let g:netrw_banner = 0")
+
 -- line numbers
-opt.relativenumber = true -- show relative line numbers
-opt.number = true -- shows absolute line number on cursor line (when relative number is on)
+opt.relativenumber = true
+opt.number = true
 
--- tabs & indentation
-opt.tabstop = 2 -- 2 spaces for tabs (prettier default)
-opt.shiftwidth = 2 -- 2 spaces for indent width
-opt.expandtab = true -- expand tab to spaces
-opt.autoindent = true -- copy indent from current line when starting new one
-opt.colorcolumn = "80" -- for better readability, highlight column 80
+-- spacing and what not
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.softtabstop = 4
+opt.expandtab = true
+opt.autoindent = true
+opt.smartindent = true
+opt.colorcolumn = "80"
 
--- line wrapping
-opt.wrap = false -- disable line wrapping
+opt.wrap = false
+opt.cursorline = true
 
--- search settings
-opt.ignorecase = true -- ignore case when searching
-opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
-
--- cursor line
-opt.cursorline = true -- highlight the current cursor line
-
--- appearance
-
--- turn on termguicolors for nightfly colorscheme to work
--- (have to use iterm2 or any other true color terminal)
 opt.termguicolors = true
-opt.background = "dark" -- colorschemes that can be light or dark will be made dark
-opt.signcolumn = "yes" -- show sign column so that text doesn't shift
+opt.background = "dark"
+opt.signcolumn = "yes"
+opt.scrolloff = 8
 
--- backspace
-opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
+opt.incsearch = true
+opt.inccommand = "split"
+opt.ignorecase = true
+opt.smartcase = true
 
--- clipboard
-opt.clipboard:append("unnamedplus") -- use system clipboard as default register
+opt.backspace = {"start", "eol", "indent"}
 
--- split windows
-opt.splitright = true -- split vertical window to the right
-opt.splitbelow = true -- split horizontal window to the bottom
+opt.splitright = true
+opt.splitbelow = true
 
--- turn off swapfile
-opt.swapfile = false
+opt.swapfile = false 
+opt.backup = false
+opt.undofile = true
+
+opt.updatetime = 60
+opt.hlsearch = true
+
+opt.mouse = "a"
+vim.g.editorconfig = true
+
+opt.clipboard:append("unnamedplus")
+
