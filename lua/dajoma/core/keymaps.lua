e4779149ac232a10c33dbaef035847571129d8ae
David Reyes <david.reyes@vestwell.com>
2025-05-29 17:42:15 -0400

    update keymaps

--- lua/dajoma/core/keymaps.lua
+++ lua/dajoma/core/keymaps.lua
@@ -1,40 +1,96 @@
--- set leader key to space
+local opts = { noremap = true, silent = true }
+
 vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
-local keymap = vim.keymap -- for conciseness
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "moves lines down in visual selection" })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "moves lines up in visual selection" })
 
----------------------
--- General Keymaps -------------------
+vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "move down in buffer with cursor centered" })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "move up in buffer with cursor centered" })
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
 
--- use jk to exit insert mode
-keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
+vim.keymap.set("v", "<", "<gv", opts)
+vim.keymap.set("v", ">", ">gv", opts)
 
--- clear search highlights
-keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
+-- the how it be paste
+vim.keymap.set("x", "<leader>p", [["_dP]])
 
--- delete single character without copying into register
--- keymap.set("n", "x", '"_x')
+-- remember yanked
+vim.keymap.set("v", "p", '"_dp', opts)
 
--- buffer navigation
-keymap.set("n", "<Tab>", "<cmd>bn<CR>", { desc = "Next buffer" }) -- next buffer
-keymap.set("n", "<S-Tab>", "<cmd>bp<CR>", { desc = "Previous buffer" }) -- previous buffer
+-- Copies or Yank to system clipboard
+vim.keymap.set("n", "<leader>Y", [["+Y]], opts)
 
--- tab indentation
-keymap.set("v", "<Tab>", ">gv", { desc = "Indent lines" })
-keymap.set("v", "<S-Tab>", ">gv", { desc = "Back indent lines" })
+-- leader d delete wont remember as yanked/clipboard when delete pasting
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
--- increment/decrement numbers
-keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" }) -- increment
-keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decrement
+-- ctrl c as escape cuz Im lazy to reach up to the esc key
+vim.keymap.set("i", "<C-c>", "<Esc>")
+vim.keymap.set("n", "<C-c>", ":nohl<CR>", { desc = "Clear search hl", silent = true })
+-- format without prettier using the built in
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
--- window management
-keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
-keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
-keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
-keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
+-- Unmaps Q in normal mode
+vim.keymap.set("n", "Q", "<nop>")
 
-keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
-keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
-keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
-keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
-keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+--Stars new tmux session from in here
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+
+-- prevent x delete from registering when next paste
+vim.keymap.set("n", "x", '"_x', opts)
+
+-- Replace the word cursor is on globally
+vim.keymap.set(
+  "n",
+  "<leader>s",
+  [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
+  { desc = "Replace word cursor is on globally" }
+)
+
+-- Executes shell command from in here making file executable
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true, desc = "makes file executable" })
+
+-- Hightlight yanking
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlight when yanking (copying) text",
+  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+  callback = function()
+    vim.hl.on_yank()
+  end,
+})
+
+-- tab stuff
+vim.keymap.set("n", "<leader>to", "<cmd>tabnew<CR>") --open new tab
+vim.keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>") --close current tab
+vim.keymap.set("n", "<leader>tn", "<cmd>tabn<CR>") --go to next
+vim.keymap.set("n", "<leader>tp", "<cmd>tabp<CR>") --go to pre
+vim.keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>") --open current tab in new tab
+
+--split management
+vim.keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })
+-- split window vertically
+vim.keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })
+-- split window horizontally
+vim.keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
+-- close current split window
+vim.keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" })
+
+-- Copy filepath to the clipboard
+vim.keymap.set("n", "<leader>fp", function()
+  local filePath = vim.fn.expand("%:~") -- Gets the file path relative to the home directory
+  vim.fn.setreg("+", filePath) -- Copy the file path to the clipboard register
+  print("File path copied to clipboard: " .. filePath) -- Optional: print message to confirm
+end, { desc = "Copy file path to clipboard" })
+
+-- Toggle LSP diagnostics visibility
+local isLspDiagnosticsVisible = true
+vim.keymap.set("n", "<leader>lx", function()
+  isLspDiagnosticsVisible = not isLspDiagnosticsVisible
+  vim.diagnostic.config({
+    virtual_text = isLspDiagnosticsVisible,
+    underline = isLspDiagnosticsVisible,
+  })
+end, { desc = "Toggle LSP diagnostics" })
